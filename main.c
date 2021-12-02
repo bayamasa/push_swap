4e69ef10ee9a3a26284ea194ab64a052bddb794d
bayamasa <mapi0213mgt@gmail.com>
2021-12-02 22:01:41 +0900

    wip

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 17:21:35 by mhirabay          #+#    #+#             */
-/*   Updated: 2021/12/02 18:40:22 by mhirabay         ###   ########.fr       */
+/*   Updated: 2021/12/02 21:51:22 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,24 @@ void	print_error(void)
 
 void	args_error_handling(int argc, char const *argv[])
 {
-	long	num;
-	int		i;
+	long long	num;
+	int			i;
+	int			*status;
 
 	if (argc <= 1)
 	{
 		print_error();
 	}
-	while (i < argc)
+	while (i < argc - 1)
 	{
-		
+		ft_atoi_error(argv[i], status);
+		if (*status == false)
+		{
+			print_error();
+			break ;
+		}
 		i++;
 	}
-	
-	num = ft_atoi(*argv[1]);
 }
 
 int	main(int argc, char const *argv[])
