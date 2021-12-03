849f1e537bcaa40e79cddf86cdf0c3c590da034f
bayamasa <mapi0213mgt@gmail.com>
2021-12-03 17:58:12 +0900

    done

--- pa.c
+++ pa.c
@@ -6,7 +6,7 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 11:41:58 by mhirabay          #+#    #+#             */
-/*   Updated: 2021/12/03 17:50:07 by mhirabay         ###   ########.fr       */
+/*   Updated: 2021/12/03 17:57:55 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,16 @@ int	pa(t_plist **a, t_plist **b)
 		return (false);
 	}
 	if (*b == NULL)
-	{
-		MYDEBUG();
 		return (true);
-	}
 	if (*a == NULL)
 	{
 		*a = ft_plstnew((*b)->num);
 		ft_plstdel_front(b);
-		return (true);
 	}
 	else
 	{
 		ft_plstaddone_front(a, *b);
 		ft_plstdel_front(b);
-		return (true);
 	}
 	return (true);
 }
